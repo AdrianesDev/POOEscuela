ed531571a81dcb03a6b03da02670fe9058ad89f0
Yahir <yahirhm1102@gmail.com>
2025-03-31 10:04:30 -0700

    Termine practica (modificado a ingles)

--- Practicas/Yahir-Hermosillo/Parcial3/Practica5/Main.java
+++ Practicas/Yahir-Hermosillo/Parcial3/Practica5/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // Crear una mascota
-        Mascota mascota1 = new Mascota("Firulais", "Perro", 3);
+        Pet pet1 = new Pet("Firulais", "Perro", 3);
         
         // Mostrar su descripcion
-        System.out.println(mascota1.descripcion());
+        System.out.println(pet1.descripcion());
         
         // Adoptar la mascota
-        mascota1.adoptar();
+        pet1.adoptar();
         
         // Mostrar descripcion despues de adoptar
-        System.out.println(mascota1.descripcion());
+        System.out.println(pet1.descripcion());
     }
 }
