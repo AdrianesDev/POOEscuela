6b78e544c0a2c43ff25b8bbc5bb64b393fa81094
Luisbm14 <luis60bf@gmail.com>
2025-04-07 09:51:31 -0700

    Agregue personajes

--- Practicas/Luis-BM/Practica.05/Marvel.py
+++ Practicas/Luis-BM/Practica.05/Marvel.py
@@ -26,6 +26,19 @@ class SpiderMan(MarvelCharacter):
     def showPower(self):
         print(f"{self.name} alias {self.alias} of {self.universe} uses his webs")
 
+class Hulk(MarvelCharacter):
+    def showPower(self):
+        print(f"{self.name} alias {self.alias} of {self.universe} use super strange")
+
+class Thor(MarvelCharacter):
+    def showPower(self):
+        print(f"{self.name} alias {self.alias} of {self.universe} use hummer")
+
+
+class BlackPanther(MarvelCharacter):
+    def showPower(self):
+        print(f"{self.name} alias {self.alias} of {self.universe} use super strange")
+
 
 def showHeroPower(character):
     character.showPower()
@@ -38,5 +51,22 @@ peter = SpiderMan(name = "Peter Parker", alias = "SpiderMan", universe = "MCU")
 peter.updateAge(newAge = 16)
 print(f"{peter.name} is {peter.getAge()} years old")
 
+bruce = Hulk(name = "Bruce Banner", alias = "Hulk", universe = "MCU")
+bruce.updateAge(newAge = 40)
+print(f"{bruce.name} is {bruce.getAge()} years old")
+
+thor = Thor(name = "Thor", alias = "Thor", universe = "MCU")
+thor.updateAge(newAge = 45)
+print(f"{thor.name} is {thor.getAge()} years old")
+
+tchalla = BlackPanther(name = "T'Challa", alias = "Black Panther", universe = "MCU")
+tchalla.updateAge(newAge = 30)
+print(f"{tchalla.name} is {tchalla.getAge()} years old")
+
+
+
 showHeroPower(tony)
 showHeroPower(peter)
+showHeroPower(bruce)
+showHeroPower(thor)
+showHeroPower(tchalla)
